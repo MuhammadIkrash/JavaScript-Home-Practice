e3dc0f6d2e266229cfbc94907f4d7d30d46adf68
Muhammad Ikrash <mikrash62@gmail.com>
2026-02-22 23:22:24 +0500

    more practice work

---
+++ chapter_2practice.js
@@ -0,0 +1,57 @@
+// chapter 2 practice question 
+
+// Q No 1
+
+// let age = prompt('Enter Age ')
+// if (age> 10 && age < 20) {
+//     console.log('Your Age is Between 10 to 20');
+    
+// } else {
+//     console.log('Your Age is not Between 10 to 20');
+// }
+
+// Q No 2
+/*
+ let age = Number(prompt('Enter Age '))
+switch (age) {
+    case 12:
+        console.log("you are kid");
+        break;
+case 22:
+        console.log("you are Young");
+        break;
+        case 82:
+        console.log("you are old");
+        break;
+    default:
+        console.log('Enter your age');
+        break;
+} */
+
+// Q No 3
+
+//  let num = Number(prompt('Enter Num '))
+//  if (num % 2 === 0 && num % 2 === 0) {
+//     console.log('num is divisible by 2 & 3 ');
+    
+//  } else {
+//     console.log('num is not divisible by 2 & 3 ');
+    
+//  }
+
+// Q No 4
+
+//  let num = Number(prompt('Enter Num '))
+//  if (num % 2 === 0 || num % 2 === 0) {
+//     console.log('num is divisible by 2 & 3 ');
+    
+//  } else {
+//     console.log('num is not divisible by 2 & 3 ');
+    
+//  }
+
+// Q No 5
+
+// let age = 12
+// let a = age>18? 'You can Drive ' : 'You cannot drive'
+// console.log(a);
---
+++ loop.js
@@ -0,0 +1,106 @@
+// loop
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+    
+// }
+
+// let sum = 0;
+// let n = Number(prompt('enter value of n'))
+// for (let i = 0; i < n; i++) {
+//     sum += (i+1)
+//     console.log(i+1);
+    
+// }
+// console.log(`Sum of first ${n}  neutral number is ${sum}`);
+
+// agar (n) ki value 5 ho
+
+// first i = 0 i + 1 = 1 
+// sum = 0 + 1(i) = 0
+
+// second i = 1 i + 1 = 2 
+// sum = 1 + 2(i) = 3
+
+// second i = 2 i + 1 = 3 
+// sum = 3 + 3(i) = 6
+
+// second i = 3 i + 1 = 4 
+// sum = 6 + 4(i) = 10
+
+// second i = 4 i + 1 = 5 
+// sum = 10 + 5(i) = 15
+
+
+// let mark = {
+// ali : 45 ,
+// jan : 40 ,
+// jarry : 35 ,
+// tom : 30 ,
+// kid : 20 ,
+// }
+
+// for in loop
+
+// for (let a in mark) {
+//    console.log(` Marks Of ${a} are ${mark[a]}`);
+    
+// }
+
+// for of loop 
+
+// for (let a of 'ali') {
+//    console.log(a);
+    
+// }
+
+
+// while loop
+
+// let num = Number(prompt('Enter Your Number'))
+// let i = 0
+// while (i< num) {
+//     console.log(i);
+//     i++;
+// }
+// do while loop
+
+// do {
+// console.log(i);
+// i++
+// }while (i< num)
+
+// loop practice question
+
+// let sum = 0 ;
+// for (let i = 0; i < 10; i++) {
+//     console.log(i + 1);
+//     sum += i + 1
+// }
+// console.log(sum );
+
+
+// let nu = Number(prompt('Number'))
+// let i = 0
+// while (i< nu) {
+//     console.log(i);
+//     i++
+// }
+// let i = 0
+// do{
+// console.log(i);
+// i++;
+// i++;
+// }while(i< nu )
+
+// let marks = {ali: 45, jan: 40, tom: 30};
+// for (const stu in marks) {
+//     console.log(stu , marks[stu]);
+// }
+
+// let arr = [5, 10, 15, 20];
+// for (const hi of arr) {
+//     console.log(hi);
+    
+// }
