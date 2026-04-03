2d49ef03947b30b9e186edb5acd91b56388008e0
Muhammad Ikrash <mikrash62@gmail.com>
2026-04-03 12:19:17 +0500

    Practice

---
+++ chapter_6practice.js
@@ -0,0 +1,26 @@
+// let run = true;
+// while (run) {
+//     let UserAge = Number(prompt('Enter Your Age'))
+//     if (UserAge <= 0) {
+//         console.error('Give Valid Value');
+//         break;
+//     }
+//     const CanDrive = (UserAge) => {
+//         return UserAge >= 18 ? true : false
+//     }
+//     if (CanDrive(UserAge)) {
+//         alert(' Yah Now You Can Drive A Car')
+//     } else {
+//         alert('You Can NOt Drive A Car . Before 18 Year')
+//     }
+//     run = confirm("Do You Want See Again")
+// }
+
+// let UserNumber = Number(prompt('Enter Your Number'))
+
+// if (UserNumber >= 4) {
+//     location.href = "https://www.youtube.com/watch?v=HzWhsTl10mw&list=PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR&index=29"
+// } 
+
+// let color = prompt('Color')
+// document.body.style.background = color
